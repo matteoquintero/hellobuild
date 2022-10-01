29ee5ec82a9f66984954226987ea632ab2326fae
Mateo Quintero <matteoquintero@gmail.com>
2022-10-01 05:51:23 -0500

    feact(back-front): the last changes

--- back/src/routes/users.ts
+++ back/src/routes/users.ts
@@ -4,15 +4,16 @@ import * as userServices from '../services/userServices'
 const router = express.Router()
 
 router.get('/', (_req:any, res:any) => {
-  res.send(userServices.getUsersNoPass())
+  res.send(userServices.getUsers())
 })
 
-router.get('/:email', (req:any, res:any) => {
-  const diary = userServices.findByEmail(req.params.email)
 
-  return (diary != null)
-    ? res.status(200).json({data:diary})
-    : res.status(404).json({data:null})
+router.get('/:email', (req:any, res:any) => {
+  const user = userServices.findByEmail(req.params.email)
+
+  return (user != null)
+    ? res.status(200).json({data:user, status:true})
+    : res.status(404).json({data:null, status:false})
 })
 
 router.post('/', (req:any, res:any) => {
@@ -21,9 +22,9 @@ router.post('/', (req:any, res:any) => {
     //En esta parte faltan muchas validaciones
     const addedUser = userServices.addUser(newUser)
 
-    res.status(201).json(addedUser)
+    res.status(201).json({data:addedUser, status:true})
   } catch (e:any) {
-    res.status(400).send(e.message)
+    res.status(400).json({data:e.message, status:false})
   }
 })
 
